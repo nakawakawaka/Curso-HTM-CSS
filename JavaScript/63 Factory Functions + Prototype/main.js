06740c355426e37d78dae4cc028f47deae1e08fd
naoki nakamura <bruno.naoki.nakamura@gmail.com>
2022-10-06 20:50:51 +0900

    factory function + prototype

---
+++ JavaScript/63 Factory Functions + Prototype/main.js
@@ -0,0 +1,31 @@
+const falar = {
+  falar() {
+    console.log(`${this.nome} esta falando.`);
+  },
+}
+
+const comer = {
+  comer() {
+    console.log(`${this.nome} esta comendo.`);
+  },
+}
+
+const beber = {
+  beber() {
+    console.log(`${this.nome} esta bebendo.`);
+  },
+}
+//const pessoaPrototype = { ...falar, ...comer, ...falar};
+const pessoaPrototype = Object.assign({}, falar, comer, beber);
+
+function criaPessoa(nome, sobrenome) {
+  return Object.create(pessoaPrototype, {
+    nome: {value: nome},
+    sobrenome: {value: sobrenome},
+  });
+}
+
+const p1 = criaPessoa('naoki', 'nakamura');
+
+
+
