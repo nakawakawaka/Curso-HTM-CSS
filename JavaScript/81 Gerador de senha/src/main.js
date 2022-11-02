79bae840c220ceb7f17243356f3e5826b66c77be
naoki nakamura <bruno.naoki.nakamura@gmail.com>
2022-11-02 20:25:48 +0900

    import para navegadores mais antigos

--- JavaScript/81 Gerador de senha/src/main.js
+++ JavaScript/81 Gerador de senha/src/main.js
@@ -1,3 +1,7 @@
+// quando precisar que o codigo rode em navegadores mais antigos que nao tem algumas funcoes
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 import formGeraSenha from './modules/formGeraSenha'
 import './Assets/Css/style.css';
 
