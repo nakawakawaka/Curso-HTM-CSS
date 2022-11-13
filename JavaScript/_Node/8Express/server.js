2bf396076085a030ed132bb857d74b491dc5e879
naoki nakamura <bruno.naoki.nakamura@gmail.com>
2022-11-13 19:58:08 +0900

    req.params req.query req.body

---
+++ JavaScript/_Node/8Express/server.js
@@ -0,0 +1,41 @@
+// mudando o script do package.json para nodemon precisamos diginar npm start no terminal para que o servidor fique assistindo as alteracoes
+
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send(`
+        <form action="/" method="POST">
+        Nome: <input type="text" name="nome">
+        <button>Enviar</button>
+        </form>
+    `);
+});
+
+app.get('/testes/:idUsuarios?/:parametro?', (req, res) => {
+    // /profile/3
+    console.log(req.params);
+    // /profile/?chave1=valor1&chave2=valor3
+    console.log(req.query);
+    res.send(req.params);
+    res.send(req.query);
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send(`O que voce enviou foi: ${req.body.nome}`);
+});
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+});
+
+
+
+
+
+
+
