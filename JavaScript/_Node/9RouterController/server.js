1fc7a7e1f5bb73779071d94c1f8ed3e7b4cbc3b9
naoki nakamura <bruno.naoki.nakamura@gmail.com>
2022-11-14 21:16:42 +0900

    routes controllers

---
+++ JavaScript/_Node/9RouterController/server.js
@@ -0,0 +1,21 @@
+// mudando o script do package.json para nodemon precisamos diginar npm start no terminal para que o servidor fique assistindo as alteracoes
+
+const express = require('express');
+const app = express();
+const routes = require('./routes')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+});
+
+
+
+
+
+
+
