1d7b5a3e52e132971af6eac9c8e021ec23e7d021
naoki nakamura <bruno.naoki.nakamura@gmail.com>
2022-11-15 19:16:09 +0900

    arquivo estatico

---
+++ JavaScript/_Node/11Estatico/server.js
@@ -0,0 +1,27 @@
+// mudando o script do package.json para nodemon precisamos diginar npm start no terminal para que o servidor fique assistindo as alteracoes
+
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(routes);
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+});
+
+
+
+
+
+
+
