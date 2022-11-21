334609b973f8ed8373f14ca6722aa4a8ba2bbe93
naoki nakamura <bruno.naoki.nakamura@gmail.com>
2022-11-21 21:27:12 +0900

    injetando conteudo e includes

--- JavaScript/_Node/16InjetandoConteudo/src/controllers/homeController.js
+++ JavaScript/_Node/16InjetandoConteudo/src/controllers/homeController.js
@@ -12,7 +12,10 @@
 //     .catch(e => console.log(e));
 
 exports.paginaInicial = (req, res, next) => {
-    res.render('index');
+    res.render('index', {
+        titulo: 'Este sera o titulo da pagina',
+        numeros: [0,1,2,3,4,5,6,7,8,9]
+    });
     return;
 }
 
