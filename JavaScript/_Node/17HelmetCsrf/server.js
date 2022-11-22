ff6f8a94d2fb7654355547042771404cca23d36b
naoki nakamura <bruno.naoki.nakamura@gmail.com>
2022-11-22 20:27:42 +0900

    express.json

--- JavaScript/_Node/17HelmetCsrf/server.js
+++ JavaScript/_Node/17HelmetCsrf/server.js
@@ -21,7 +21,9 @@ const csrf = require('csurf');
 const { middlewareGlobal, checkCsrfError , csrfMiddleware} = require('./src/middleware/middleware')
 
 app.use(helmet());
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOption = session({
