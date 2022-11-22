0274923d6594d0435af18b9b2fc551333c53cc7b
naoki nakamura <bruno.naoki.nakamura@gmail.com>
2022-11-22 20:32:21 +0900

    limpando codigos desnecessarios

--- JavaScript/_Node/17HelmetCsrf/routes.js
+++ JavaScript/_Node/17HelmetCsrf/routes.js
@@ -3,26 +3,12 @@ const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const contatoController = require('./src/controllers/contatoController');
 
-function meuMiddleware(req, res, next) {
-    res.session = { nome: 'naoki', sobrenome: 'nakamura' }
-    console.log();
-    console.log('Passei no seu middleware');
-    console.log();
-    next()
-}
-
 // Rotas da home
-route.get('/', meuMiddleware, homeController.paginaInicial, function(req, res, next) {
-    console.log();
-    console.log('ainda estou aqui');
-    console.log(`'Ultimo middleware' -> Olha o que tem na req.session.sobrenome ${req.session.sobrenome}`);
-});
+route.get('/', homeController.paginaInicial);
 route.post('/', homeController.trataPost);
 
 // Rotas de contato
 route.get('/contato', contatoController.paginaInicial);
-
-
 
 
 module.exports = route;
--- JavaScript/_Node/17HelmetCsrf/src/controllers/homeController.js
+++ JavaScript/_Node/17HelmetCsrf/src/controllers/homeController.js
@@ -1,16 +1,3 @@
-// const HomeModel = require('../models/HomeModel');
-
-// HomeModel.create({ 
-//     titulo: 'Outro Titulo de teste',
-//     descricao: 'Outro Descricao de teste'
-// })
-//     .then(dados => console.log(dados))
-//     .catch(e => console.log(e));
-
-// HomeModel.find()
-//     .then(dados => console.log(dados))
-//     .catch(e => console.log(e));
-
 exports.paginaInicial = (req, res, next) => {
     res.render('index', {
         titulo: 'Este sera o titulo da pagina',
